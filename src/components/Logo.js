a433dc388866411bbea7c6686ba2d83f9d1e7335
Gabriel L <121911166+Gabnix@users.noreply.github.com>
2023-10-05 02:35:36 +0000

    up until About page

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,4 +1,3 @@
-
 import Link from 'next/link'
 import React from 'react'
 import {motion} from 'framer-motion';
@@ -14,7 +13,7 @@ const Logo = () => {
                 backgroundColor:["#121212", "rgba(131,58,180,1)","rgba(253,29,29,1)","rgba(252,176,69,1)","rgba(131,58,180,1)", "#121212"],
                 transition:{duration:1, repeat: Infinity}
             }}
-            >CB</MotionLink>
+            >GL</MotionLink>
         </div>
     )
 }
